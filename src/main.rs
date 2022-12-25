7f96e0b14481917c3299abadf7d07f09471fabbe
EC2 Default User <ec2-user@ip-172-31-27-232.us-west-2.compute.internal>
2022-12-25 06:10:01 +0000

    Got initial rust version of the bot working with a few basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,85 @@
+use twitch_irc::message::ServerMessage;
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::TwitchIRCClient;
 use twitch_irc::{ClientConfig, SecureTCPTransport};
+use std::time::Duration;
+use tokio::{task, time};
 
 #[tokio::main]
 pub async fn main() {
     // default configuration is to join chat as anonymous.
-    let config = ClientConfig::default();
+
+    let login_name = std::env::var("TWITCH_NAME").ok().unwrap();
+    let oauth_token = std::env::var("TWITCH_TOKEN").ok().unwrap();
+
+    let config = ClientConfig::new_simple(
+        StaticLoginCredentials::new(login_name, Some(oauth_token))
+    );
     let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let x = client.clone();
 
     // first thing you should do: start consuming incoming messages,
     // otherwise they will back up.
     let join_handle = tokio::spawn(async move {
         while let Some(message) = incoming_messages.recv().await {
-            println!("Received message: {:?}", message);
+            //println!("Received message: {:?}", message);
+
+            match message {
+                ServerMessage::Privmsg(msg) => {
+                    println!("(#{}) {}: {}", msg.channel_login, msg.sender.name, msg.message_text);
+
+                    // Discord
+                    if msg.message_text.to_lowercase().contains("!discord") {
+                        x.say("rockzombie2".to_owned(), "Join the Space Pirates Discord! https://discord.gg/ErM35TPK2D".to_owned()).await.unwrap();
+                    }
+
+                    // Operating System
+                    if msg.message_text.to_lowercase().contains("!os") {
+                        x.say("rockzombie2".to_owned(), "I run Windows as my host OS (for gaming) and screencapture my !laptop which dual-boots FreeBSD and Windows 11.".to_owned()).await.unwrap();
+                    }
+
+                    // Laptop
+                    if msg.message_text.to_lowercase().contains("!laptop") {
+                        x.say("rockzombie2".to_owned(), "I dual boot FreeBSD and Windows 11 on a ThinkPad P15v Gen 3 AMD (15\") laptop.".to_owned()).await.unwrap();
+                    }
+
+                    // Window Manager
+                    if msg.message_text.to_lowercase().contains("!wm") {
+                        x.say("rockzombie2".to_owned(), "I use the suckless window manager, DWM.".to_owned()).await.unwrap();
+                    }
+
+                    // dotfiles
+                    if msg.message_text.to_lowercase().contains("!dotfiles") {
+                        x.say("rockzombie2".to_owned(), "You can find my dotfiles on !github: https://github.com/cpadilla/dotfiles".to_owned()).await.unwrap();
+                    }
+
+                    // Github
+                    if msg.message_text.to_lowercase().contains("!github") {
+                        x.say("rockzombie2".to_owned(), "https://github.com/cpadilla".to_owned()).await.unwrap();
+                    }
+
+                    // socials
+                    if msg.message_text.to_lowercase().contains("!socials") ||
+                        msg.message_text.to_lowercase().contains("!tiktok") ||
+                        msg.message_text.to_lowercase().contains("!youtube") ||
+                        msg.message_text.to_lowercase().contains("!twitter") {
+                        x.say("rockzombie2".to_owned(), "Follow me on social media and checkout my !blog! https://twitter.com/rockzombie2 https://www.tiktok.com/@rockzombie2 https://youtube.com/rockzombie2".to_owned()).await.unwrap();
+                    }
+
+                    // blog
+                    if msg.message_text.to_lowercase().contains("!website") || msg.message_text.to_lowercase().contains("!blog") {
+                        x.say("rockzombie2".to_owned(), "Reflections - https://christofer.rocks/".to_owned()).await.unwrap();
+                    }
+
+                },
+                ServerMessage::Whisper(msg) => {
+                    println!("(w) {}: {}", msg.sender.name, msg.message_text);
+                },
+                _ => {}
+            }
+
         }
     });
 
@@ -26,4 +92,19 @@ pub async fn main() {
     // keep the tokio executor alive.
     // If you return instead of waiting the background task will exit.
     join_handle.await.unwrap();
+
+    let y = client.clone();
+
+    // Periodically remind chat to join socials
+    let reminders = task::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(900));
+        loop {
+            // Wait 15 minutes
+            interval.tick().await;
+            y.say("rockzombie2".to_owned(), "Join the Space Pirates Discord! https://discord.gg/ErM35TPK2D".to_owned()).await.unwrap();
+        }
+    });
+
+    // TODO: run these concurrently
+    reminders.await.unwrap();
 }
